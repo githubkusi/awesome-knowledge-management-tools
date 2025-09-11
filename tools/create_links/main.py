a2ae394314a943e1260542174953aea73886ea24
kusi <git@titlis.org>
2025-09-11 11:01:26 +0200

    fix tooling

--- tools/create_links/main.py
+++ tools/create_links/main.py
@@ -2,12 +2,12 @@ import pyperclip
 
 
 def main():
-    name = input("Github org/repo: ")
+    name = input("Github org/repo (without https://github.com/): ")
     branch = input("Branch: ")
     d = {
         "stars": f"https://img.shields.io/github/stars/{name}?label=%20",
         "contributors": f"https://img.shields.io/github/contributors/{name}?label=%20",
-        "last commit": f"https://img.shields.io/github/last-commit/{name}/{branch}?label = %20",
+        "last commit": f"https://img.shields.io/github/last-commit/{name}/{branch}?label=%20",
         "source language": f"https://img.shields.io/github/languages/top/{name}",
         "license": f"https://img.shields.io/github/license/{name}?label=%20"
     }
